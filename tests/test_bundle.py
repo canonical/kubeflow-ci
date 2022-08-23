6edddb4a1f03c0dc08a4264537d7861ddc65094b
Natasha Ho <natasha.ho@canonical.com>
2022-08-23 18:02:09 -0400

    use ops_test fast_forward

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -15,7 +15,7 @@ PASSWORD = "foobar"
 @pytest.mark.v1dot6
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_deploy_1dot6(ops_test, lightkube_client, deploy_cmd):
+async def test_deploy_1dot6(ops_test: OpsTest, lightkube_client, deploy_cmd):
     print(f"Deploying bundle to {ops_test.model_full_name} using cmd '{deploy_cmd}'")
     rc, stdout, stderr = await ops_test.run(*shlex.split(deploy_cmd))
 
@@ -45,7 +45,7 @@ async def test_deploy_1dot6(ops_test, lightkube_client, deploy_cmd):
 @pytest.mark.v1dot4
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_deploy_1dot4(ops_test, lightkube_client, deploy_cmd):
+async def test_deploy_1dot4(ops_test: OpsTest, lightkube_client, deploy_cmd):
     print(f"Deploying bundle to {ops_test.model_full_name} using cmd '{deploy_cmd}'")
     rc, stdout, stderr = await ops_test.run(*shlex.split(deploy_cmd))
 
@@ -56,17 +56,15 @@ async def test_deploy_1dot4(ops_test, lightkube_client, deploy_cmd):
         timeout=1800,
     )
 
-    await ops_test.model.set_config({"update-status-hook-interval": "15s"})
-    istio_gateway_role_name = "istio-ingressgateway-operator"
-
     print("Patch role for istio-gateway")
-    new_policy_rule = PolicyRule(verbs=["*"], apiGroups=["*"], resources=["*"])
-    this_role = lightkube_client.get(Role, istio_gateway_role_name)
-    this_role.rules.append(new_policy_rule)
-    lightkube_client.patch(Role, istio_gateway_role_name, this_role)
+    async with ops_test.fast_forward(fast_interval="15s"):
+        istio_gateway_role_name = "istio-ingressgateway-operator"
+        new_policy_rule = PolicyRule(verbs=["*"], apiGroups=["*"], resources=["*"])
+        this_role = lightkube_client.get(Role, istio_gateway_role_name)
+        this_role.rules.append(new_policy_rule)
 
-    time.sleep(50)
-    await ops_test.model.set_config({"update-status-hook-interval": "5m"})
+        lightkube_client.patch(Role, istio_gateway_role_name, this_role)
+        time.sleep(50)
 
     print("Waiting for bundle to be ready")
     await ops_test.model.wait_for_idle(

d3cd936800f524406de16e738d6bb61f6717baf9
Andrew Scribner <ca.scribner+1@gmail.com>
2022-10-13 10:20:30 -0400

    add procedure and script to split up a review of our issue backlog (#32)
    * add procedure and script to split up a review of our issue backlog
    
    This is some simple instructions and a script to take out entire issue backlog and split it up, by repository, into groups of repositories with roughly the same number of issues.  This makes it easy for us to then each take a group of a few repos, review the open issues, and maintain our backlog.
    
    See readme.md for more details

---
+++ scripts/backlog-review/group_issues.py
@@ -0,0 +1,50 @@
+import json
+
+import typer
+
+
+def main(issue_file: str, n_groups: int):
+    """
+    Splits a list of repos into n_groups, where each group has roughly the same number of issues.
+
+    Args:
+        issue_file: filename to a file of JSON format with structure:
+            [
+                {
+                    "issues": {"totalCount":7},
+                    "name":"kubeflow-ci"
+                },
+                {   "issues": ...
+                },
+                ...
+            ]
+        n_groups: Number of roughly equal groups to split things into
+
+    Return:
+        Prints to screen a list of groups of repos, in JSON format
+    """
+    try:
+        with open(issue_file) as f:
+            repos = json.load(f)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(
+            f"Could not find input issue file '{issue_file}' - does this file exist?"
+        ) from e
+
+    # Sort repos by number of issues
+    repos.sort(key=lambda x: x["issues"]["totalCount"], reverse=True)
+
+    # Split into n_groups, adding the next repo to the group with the fewest issues
+    groups = [{"n_issues": 0, "repos": []} for _ in range(n_groups)]
+
+    for repo in repos:
+        groups[0]["repos"].append(repo["name"])
+        groups[0]["n_issues"] += repo["issues"]["totalCount"]
+        groups.sort(key=lambda x: x["n_issues"])
+
+    # Print to screen
+    print(json.dumps(groups, indent=2))
+
+
+if __name__ == "__main__":
+    typer.run(main)

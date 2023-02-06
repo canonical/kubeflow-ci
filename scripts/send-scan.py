ec67a51d3610c369c4dd332066494f9cc1ca50d3
Ivan Chvets <ivan.chvets@canonical.com>
2023-02-06 16:38:24 -0500

    feat: auto build and scan https://github.com/canonical/notebook-operators/issues/188
    Summary of changes:
    - Fixed directory processing.
    - Added edge case handling.

--- scripts/send-scan.py
+++ scripts/send-scan.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
 
-
 from pathlib import Path
+import os
 import sys
 import json
 
 input = sys.argv[1]
 input_path = Path(input)
 
+input_dir = ""
 FILE_LIST = []
 if input_path.is_dir():
     # directory is supplied, retrieve list of files
-    print("dir")
+    FILE_LIST = os.listdir(input_path)
+    input_dir = input + "/"
 elif input_path.is_file():
     FILE_LIST.append(input)
 
 for file in FILE_LIST:
     print(f"Processing report in: {file}")
-    with open(file, "r") as json_file:
+    filename = input_dir + file
+    with open(filename, "r") as json_file:
         data = json.load(json_file)
     artifact = data['ArtifactName']
-    results = data['Results'][0]
-    if "Vulnerabilities" not in results or len(results['Vulnerabilities']) == 0:
-        # no vulnerabilities found, skip this report
+
+    if "Results" not in data:
+        # no scan results found, skip this report
         continue
 
-    vuln_list = results['Vulnerabilities']
-    for vuln in vuln_list:
-        record_name = str(artifact + " " + vuln['PkgName'] + " " + vuln['VulnerabilityID'])
-        record_data = vuln
-        print(f"Name: {record_name}")
-        # send record
+    for result in data['Results']:
+        if "Vulnerabilities" not in result or len(result['Vulnerabilities']) == 0:
+            # no vulnerabilities found, skip this report
+            continue
 
+        vuln_list = result['Vulnerabilities']
+        for vuln in vuln_list:
+            record_name = str(artifact + " " + vuln['PkgName'] + " " + vuln['VulnerabilityID'])
+            record_data = vuln
+            print(f"Send record with name: {record_name}")
+            # send record
 
-        

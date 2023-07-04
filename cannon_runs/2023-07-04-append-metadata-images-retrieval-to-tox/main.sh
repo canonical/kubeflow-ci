ad0e511780538cc70a2f6367888c9924aa94d7bb
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-04 17:04:43 -0400

    feat: cannon run to update tox.ini
    Summary of changes:
    - Updated cannon run script to append command with separate script
      instead of commands.

--- cannon_runs/2023-07-04-append-metadata-images-retrieval-to-tox/main.sh
+++ cannon_runs/2023-07-04-append-metadata-images-retrieval-to-tox/main.sh
@@ -10,7 +10,7 @@ echo "
 allowlist_externals =
   bash
   git
-commands = |
-        git clone --branch kf-3783-feat-metatdata-images https://github.com/canonical/kubeflow-ci.git
-        bash -c 'bash ./scripts/images/get-metadata-images.sh '\>' > metadata-images.txt && cat metadata-images.txt'
+commands =
+	git clone https://github.com/canonical/kubeflow-ci.git
+	bash -c 'bash ./kubeflow-ci/scripts/images/get-metadata-images.sh > metadata-images.txt && cat metadata-images.txt'
 " >> tox.ini

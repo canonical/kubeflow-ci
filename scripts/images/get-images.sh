deaa8afcb126172d5606521a8c947badcf22aa01
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-09 10:45:08 -0400

    feat: bundle scan
    Summary of changes:
    - Fixed syntax error.

--- scripts/images/get-images.sh
+++ scripts/images/get-images.sh
@@ -4,6 +4,6 @@ IMAGES=()
 REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique))
 for REPO in "${REPOS[@]}"; do
   echo "git clone https://github.com/canonical/$REPO"
-  IMAGES=+($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries[].value'  {} \; | jq -r '."upstream-source"'))
+  IMAGES+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries[].value'  {} \; | jq -r '."upstream-source"'))
   IMAGES+=($($REPO/tools/get-images.sh))
 done

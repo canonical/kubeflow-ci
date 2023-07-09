4657849b04ed41f0a31d5a9a540e7452f99aea5f
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-09 12:08:08 -0400

    fix: updated find command

--- scripts/images/get-images.sh
+++ scripts/images/get-images.sh
@@ -3,11 +3,9 @@ BUNDLE_FILE=$1
 IMAGES=()
 REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique))
 for REPO in "${REPOS[@]}"; do
-  echo "Clone https://github.com/canonical/$REPO"
   git clone https://github.com/canonical/$REPO
   # get charm's images from metadata
-  echo "Get charm's images"
-  IMAGES+=($(find "$REPO" -type f -name metadata.yaml -exec yq '.resources | to_entries[].value'  {} \; | jq -r '."upstream-source"'))
+  IMAGES+=($(find kubeflow-dashboard-operator -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
   # get workload images
   #IMAGES+=($($REPO/tools/get-images.sh))
 done

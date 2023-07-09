452289ae35fb4186aac773e57d6313249a9a9cd0
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-09 10:53:04 -0400

    feat: bundle scan
    Summary of changes:
    - Enabled repos checkout.

--- scripts/images/get-images.sh
+++ scripts/images/get-images.sh
@@ -3,7 +3,9 @@ BUNDLE_FILE=$1
 IMAGES=()
 REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique))
 for REPO in "${REPOS[@]}"; do
-  echo "git clone https://github.com/canonical/$REPO"
+  git clone https://github.com/canonical/$REPO
+  # get charm's images from metadata
   IMAGES+=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries[].value'  {} \; | jq -r '."upstream-source"'))
-  IMAGES+=($($REPO/tools/get-images.sh))
+  # get workload images
+  #IMAGES+=($($REPO/tools/get-images.sh))
 done

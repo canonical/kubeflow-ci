84a46fe74ce1c5ef872bd49af3ebf3876380114f
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-07-11 16:46:45 -0400

    Kf 3783 feat redesign scripts (#82)
    Common CI scripts that collect images for each repository in given bundle.
    Use as stand alone scripts or as part of workflows.
    
    get-all-images.sh - retrieves all images managed by charm AND by chams workload using get-metadata-images.sh and repository specific get-images scripts.
    get-metadata-images.sh - retrieves images from metadata of charms.
    
    Summary of changes:
    
    - Scripts to get images and to scan for CVEs.
    - Enabled repos checkout.
    - Added images output.
    - Modified script to output list.
    - Removed not needed cannon run.
    - Enabled getting images from repos.

---
+++ scripts/images/get-all-images.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+BUNDLE_FILE=$1
+RELEASE=$2
+IMAGES=()
+REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique))
+for REPO in "${REPOS[@]}"; do
+  git clone https://github.com/canonical/$REPO
+  # get charm's images from metadata
+  cd $REPO
+  IMAGES+=($(get-metadata-images.sh))
+  # get workload images
+  IMAGES+=($($REPO/tools/get-images-$RELEASE.sh))
+  cd -
+done
+printf "%s\n" "${IMAGES[@]}"

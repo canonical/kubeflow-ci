39c106b95600ade7c58f75a958b8b4f547130c72
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-12 19:42:09 -0400

    fix: updated script call to ensure proper execution

--- scripts/images/get-all-images.sh
+++ scripts/images/get-all-images.sh
@@ -6,7 +6,9 @@ REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique)
 for REPO in "${REPOS[@]}"; do
   git clone https://github.com/canonical/$REPO
   cd $REPO
-  IMAGES+=($($REPO/tools/get-images-$RELEASE.sh))
-  cd -
+  if test -f "./tools/get-images-$RELEASE.sh"; then
+    IMAGES+=($(./tools/get-images-$RELEASE.sh))
+  fi
+  cd - > /dev/null
 done
 printf "%s\n" "${IMAGES[@]}"

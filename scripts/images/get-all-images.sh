4cf28ec68277854ecfd5e20039586ef056949ff9
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-07-12 14:58:08 -0400

    Kf 3783 feat redesign scripts (#83)
    Re-design of script(s) to gather all images.
    Scripts can be used to collect image information for airgapped environment or as part of automated workflow for CVE scanning.
    
    Latest re-design update is to remove collection of image URL for metadata, because it is done in each repository's script instead to ensure proper branch is used.
    
    Summary of changes:
    - Redesign scripts to be re-usable for other taks, eg. airgapped environment setup.
    - Added release to script
    - Fix bug in directory handling
    - Fixed script path
    - Removed metadata images retrieval

--- scripts/images/get-all-images.sh
+++ scripts/images/get-all-images.sh
@@ -5,10 +5,7 @@ IMAGES=()
 REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique))
 for REPO in "${REPOS[@]}"; do
   git clone https://github.com/canonical/$REPO
-  # get charm's images from metadata
   cd $REPO
-  IMAGES+=($(get-metadata-images.sh))
-  # get workload images
   IMAGES+=($($REPO/tools/get-images-$RELEASE.sh))
   cd -
 done

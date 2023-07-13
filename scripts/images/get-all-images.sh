09918d46a8570aa0e2cbeb7642503f5bf4674a4c
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-13 09:58:34 -0400

    feat: redesign of script
    Summary of changes:
    - Get all images script is re-designed to work with updated bundle.yaml
      file that contains github branches.

--- scripts/images/get-all-images.sh
+++ scripts/images/get-all-images.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
+#
+# This script parses given bundle file for github repositories and branches. Then checks out each
+# charm's repository one by one using specified branch and collects images refered by that charm
+# using that repository's image collection script
+#
 BUNDLE_FILE=$1
-RELEASE=$2
 IMAGES=()
-REPOS=($(grep _github_repo_name $BUNDLE_FILE | awk '{print $2}' | sort --unique))
-for REPO in "${REPOS[@]}"; do
+REPOS_BRANCHES=($(yq -r '.applications[] | to_json' $BUNDLE_FILE | jq -r 'select(._github_repo_name) | "\(._github_repo_name):\(._github_repo_branch)"' | sort --unique))
+for REPO_BRANCH in "${REPOS_BRANCHES[@]}"; do
+  IFS=: read -r REPO BRANCH <<< "$REPO_BRANCH"
   git clone https://github.com/canonical/$REPO
   cd $REPO
-  IMAGES+=($($REPO/tools/get-images-$RELEASE.sh))
-  cd -
+  git checkout $BRANCH tmp
+  IMAGES+=($(./tools/get-images.sh))
+  cd - > /dev/null
 done
 printf "%s\n" "${IMAGES[@]}"

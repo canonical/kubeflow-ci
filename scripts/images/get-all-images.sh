60d561ad202df260736d8d68ff115c9fdfabd8c5
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-13 13:26:33 -0400

    fix: added parsiong of dependecy repos
    Summary of changes:
    - Added parsing of dependecy repositories to retrieve workload images
      from metadata.yaml files of dependencies repositories.

--- scripts/images/get-all-images.sh
+++ scripts/images/get-all-images.sh
@@ -6,6 +6,7 @@
 #
 BUNDLE_FILE=$1
 IMAGES=()
+# retrieve all repositories and branches for CKF
 REPOS_BRANCHES=($(yq -r '.applications[] | to_json' $BUNDLE_FILE | jq -r 'select(._github_repo_name) | "\(._github_repo_name):\(._github_repo_branch)"' | sort --unique))
 for REPO_BRANCH in "${REPOS_BRANCHES[@]}"; do
   IFS=: read -r REPO BRANCH <<< "$REPO_BRANCH"
@@ -15,4 +16,17 @@ for REPO_BRANCH in "${REPOS_BRANCHES[@]}"; do
   IMAGES+=($(bash ./tools/get-images.sh))
   cd - > /dev/null
 done
+
+# retrieve all respositories and branches for dependencies
+DEP_REPOS_BRANCHES=($(yq -r '.applications[] | to_json' $BUNDLE_FILE | jq -r 'select(._github_dependency_repo_name) | "\(._github_dependency_repo_name):\(._github_dependency_repo_branch)"' | sort --unique))
+for REPO_BRANCH in "${REPOS_BRANCHES[@]}"; do
+  IFS=: read -r REPO BRANCH <<< "$REPO_BRANCH"
+  git clone https://github.com/canonical/$REPO
+  cd $REPO
+  git checkout -b tmp $BRANCH
+  # for dependencies only retrieve workload containers from metadata.yaml
+  IMAGES+=($(find -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
+  cd - > /dev/null
+done
+
 printf "%s\n" "${IMAGES[@]}"

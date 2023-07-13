df3918f60e31d1d5cd9838920cdb2ec45f57ea82
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-13 10:48:57 -0400

    feat: script to collect all images in bundle
    Summary of changes:
    - Re-designed script to collect all images used in the bundle based on
      github repo and branch.

--- scripts/images/get-all-images.sh
+++ scripts/images/get-all-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #
 # This script parses given bundle file for github repositories and branches. Then checks out each
-# charm's repository one by one using specified branch and collects images refered by that charm
+# charm's repository one by one using specified branch and collects images referred by that charm
 # using that repository's image collection script
 #
 BUNDLE_FILE=$1
@@ -11,8 +11,8 @@ for REPO_BRANCH in "${REPOS_BRANCHES[@]}"; do
   IFS=: read -r REPO BRANCH <<< "$REPO_BRANCH"
   git clone https://github.com/canonical/$REPO
   cd $REPO
-  git checkout $BRANCH tmp
-  IMAGES+=($(./tools/get-images.sh))
+  git checkout -b tmp $BRANCH
+  IMAGES+=($(bash ./tools/get-images.sh))
   cd - > /dev/null
 done
 printf "%s\n" "${IMAGES[@]}"

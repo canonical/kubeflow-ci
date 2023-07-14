cbe1fcf74d271eacb6ac0b961ec8c5c00af29896
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-07-14 16:24:49 -0400

    feat script to collect all images in bundle (#86)
    feat: script to collect all images in bundle
    
    Script that uses repositories and branches specified in updated bundle file to retrieve container images managed by each charm and/or its workload.
    This script parses given bundle file for github repositories and branches. Then checks out each charm's repository one by one using specified branch and collects images refered by that charm using that repository's image collection script
    
    Summary of changes:
    
    Re-designed script to collect all images used in the bundle based on github repo and branch.

--- scripts/images/get-metadata-images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# get charm's images from metadata
-IMAGES=()
-IMAGES=($(find $REPO -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
-printf "%s\n" "${IMAGES[@]}"

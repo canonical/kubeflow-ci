8c3b93a7afeac18d976dba26a626984bbc1d2295
Ivan Chvets <ivan.chvets@canonical.com>
2023-08-01 15:17:19 -0400

    fix: added debug log

--- scripts/images/scan-images.sh
+++ scripts/images/scan-images.sh
@@ -56,6 +56,10 @@ for IMAGE in "${IMAGE_LIST[@]}"; do
       echo "$IMAGE,$BASE,$NUM_CRITICAL,$NUM_HIGH,$NUM_MEDIUM,$NUM_LOW" >> $SCAN_SUMMARY_FILE
     fi
     docker rmi $IMAGE
+    docker images
+    docker ps -a
+    echo "Removed image"
+    df . -h
 done
 
 if [ "$REPORT_TOTALS" = true ]; then

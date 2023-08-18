a95e2aa47fd917ba197170efc958beaaf35eec89
Ivan Chvets <ivan.chvets@canonical.com>
2023-08-18 11:40:57 -0400

    feat: generate summary per class
    Summary of changes:
    - Added script that generates CVE count summary per class, i.e. for OS
      and LANG packages classes in Trivy reports.
    - Modified main shell script to properly format report and call new
      script.

---
+++ scripts/images/get-summary.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+#
+
+"""Script to process Trivy vulnerability scans reports and produce summary."""
+
+import argparse
+import json
+from pathlib import Path
+
+
+def parse_json(filename):
+    """Parse JSON file"""
+    record_list = []
+    with open(filename, "r") as json_file:
+        data = json.load(json_file)
+
+    artifact = data["ArtifactName"]
+    if "OS" in data["Metadata"]:
+        base = f"{data['Metadata']['OS']['Family']}:{data['Metadata']['OS']['Name']}"
+    else:
+        base = "N/A"
+
+    if "Results" not in data:
+        # no scan results found, skip this report
+        print(f"No results in report {filename}")
+        return []
+
+    for result in data["Results"]:
+        if "Vulnerabilities" not in result or len(result["Vulnerabilities"]) == 0:
+            # no vulnerabilities found, skip this report
+            continue
+        package_type = result["Class"]
+        vuln_list = result["Vulnerabilities"]
+        for vuln in vuln_list:
+            record_list.append(
+                {
+                    "artifact": artifact,
+                    "base": base,
+                    "class": package_type,
+                    "severity": vuln["Severity"],
+                }
+            )
+
+    return record_list
+
+
+def main(report_path):
+    input_path = Path(report_path)
+
+    file_list = []
+    if input_path.is_dir():
+        # directory is supplied, retrieve list of files
+        file_list = list(input_path.iterdir())
+    elif input_path.is_file():
+        file_list.append(input_path)
+    else:
+        print(f"Invalid input {report_path} supplied")
+        return
+
+    if not file_list:
+        print(f"Failed to retrieve list of files from {report_path}")
+        return
+
+    for file in file_list:
+        if file.suffix == ".json":
+            records = parse_json(file)
+        else:
+            continue
+
+        # create and output summary
+        lang_pkgs = {"CRITICAL": 0, "HIGH": 0, "MEDIUM": 0, "LOW": 0, "UNKNOWN": 0}
+        os_pkgs = {"CRITICAL": 0, "HIGH": 0, "MEDIUM": 0, "LOW": 0, "UNKNOWN": 0}
+        for record in records:
+            if record["class"] == "os-pkgs":
+                os_pkgs[record["severity"]] = os_pkgs[record["severity"]] + 1
+            if record["class"] == "lang-pkgs":
+                lang_pkgs[record["severity"]] = lang_pkgs[record["severity"]] + 1
+
+        print(f"{record['artifact']},{record['base']},{os_pkgs['CRITICAL']},{os_pkgs['HIGH']},{os_pkgs['MEDIUM']},{os_pkgs['LOW']},{lang_pkgs['CRITICAL']},{lang_pkgs['HIGH']},{lang_pkgs['MEDIUM']},{lang_pkgs['LOW']}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--report-path")
+    args = parser.parse_args()
+    main(args.report_path)
--- scripts/images/scan-images.sh
+++ scripts/images/scan-images.sh
@@ -10,7 +10,6 @@
 FILE=$1
 TRIVY_REPORTS_DIR="trivy-reports"
 TRIVY_REPORT_TYPE="json"
-REPORT_TOTALS=false
 
 if [ -d "$TRIVY_REPORTS_DIR" ]; then
     echo "WARNING: $TRIVY_REPORTS_DIR directory already exists. Some reports might not be generated."
@@ -22,7 +21,7 @@ DATE=$(date +%F)
 SCAN_SUMMARY_FILE="scan-summary.csv"
 if [ ! -f $SCAN_SUMMARY_FILE ]; then
     # create header for scan summary file, if it does not exist
-    echo "IMAGE,BASE,CRITICAL,HIGH,MEDIUM,LOW " >> $SCAN_SUMMARY_FILE
+    echo "IMAGE,BASE,OS-CRITICAL,OS-HIGH,OS-MEDIUM,OS-LOW,LANG-CRITICAL,LANG-HIGH,LANG-MEDIUM,LANG-LOW" >> $SCAN_SUMMARY_FILE
 fi
 
 # create directory for trivy reports
@@ -48,23 +47,11 @@ for IMAGE in "${IMAGE_LIST[@]}"; do
     docker run -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:`pwd` -w `pwd` --name=scanner aquasec/trivy image --timeout 30m -f $TRIVY_REPORT_TYPE -o $TRIVY_REPORT --ignore-unfixed $IMAGE
     if [ "$TRIVY_REPORT_TYPE" = "json" ]; then
       # for JSON type retrieve severity counts
-      NUM_CRITICAL=$(grep CRITICAL $TRIVY_REPORT | wc -l)
-      NUM_HIGH=$(grep HIGH $TRIVY_REPORT | wc -l)
-      NUM_MEDIUM=$(grep MEDIUM $TRIVY_REPORT | wc -l)
-      NUM_LOW=$(grep LOW $TRIVY_REPORT | wc -l)
-      BASE=$(cat $TRIVY_REPORT | jq '.Metadata.OS | "\(.Family):\(.Name)"' | sed 's/"//g')
-      echo "$IMAGE,$BASE,$NUM_CRITICAL,$NUM_HIGH,$NUM_MEDIUM,$NUM_LOW" >> $SCAN_SUMMARY_FILE
+      get-summary.py --report-path $TRIVY_REPORT >> $SCAN_SUMMARY_FILE
     fi
     docker rmi $IMAGE
     docker rm -f $(docker ps -a -q)
     df . -h
 done
 
-if [ "$REPORT_TOTALS" = true ]; then
-    NUM_CRITICAL=$(grep CRITICAL "$TRIVY_REPORTS_DIR/*" | wc -l)
-    NUM_HIGH=$(grep HIGH "$TRIVY_REPORTS_DIR/*" | wc -l)
-    NUM_MEDIUM=$(grep MEDIUM "$TRIVY_REPORTS_DIR/*" | wc -l)
-    NUM_LOW=$(grep LOW "$TRIVY_REPORTS_DIR/*" | wc -l)
-    echo ",Totals:,$NUM_CRITICAL,$NUM_HIGH,$NUM_MEDIUM,$NUM_LOW" >> $SCAN_SUMMARY_FILE
-fi
 cat $SCAN_SUMMARY_FILE

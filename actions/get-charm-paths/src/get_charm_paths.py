ac10794fd14ae495fd0fd66969f672589da49fd2
Kimonas Sotirchos <kimwnasptd@gmail.com>
2024-11-27 19:20:37 +0200

    Extend logic for detecting charmcraft.yaml
    Extend the logic to also detect thet presence of a charm if a
    charmcraft.yaml file is found, and not only based on the metadata.yaml
    files.
    
    This is needed since newer versions of charmcraft are not creating
    metadata.yaml files anymore.

--- actions/get-charm-paths/src/get_charm_paths.py
+++ actions/get-charm-paths/src/get_charm_paths.py
@@ -3,59 +3,86 @@ import argparse
 import os
 from pathlib import Path
 
-
 OUTPUT_VARIABLE_NAME = "charm_paths"
+
 
 def find_charms_in_dir(base_dir: str = "./", charms_subdir="charms"):
     """
     Finds paths to charm directories in base_dir.
 
-    Directories are identified as charm directories by the presence of a "metadata.yaml" or "metadata.yml" file.
+    Directories are identified as charm directories by the presence of a
+    "metadata.yaml", "metadata.yml" or "charmcraft.yaml" file.
 
     Returns:
-    * If any subdirectories of base_dir/charms_subdir/ are charm directories, then the list of these directories is returned.
-    * If not, then if the base directory itself is a charm directory, that is returned.
+    * If any subdirectories of base_dir/charms_subdir/ are charm directories,
+      then the list of these directories is returned.
+    * If not, then if the base directory itself is a charm directory, that is
+      returned.
     * Otherwise, returns []
 
-	Directories returned are alphabetically sorted.
+        Directories returned are alphabetically sorted.
     """
-    base_dir = Path(base_dir)
-    charms_dir = base_dir / charms_subdir
-    
-    metadata_files = list(charms_dir.glob("*/metadata.yaml")) + list(charms_dir.glob("*/metadata.yml"))
-    
-    if not metadata_files:
-        # No nested charm directories found, check if the top level dir is a charm
-        metadata_files = list(base_dir.glob("metadata.yaml")) + list(base_dir.glob("metadata.yml"))
+    root_dir = Path(base_dir)
+    charms_dir = root_dir / charms_subdir
 
-    # Return the parent directory of the relevant metadata_files and remove duplicates
-    charm_dirs = sorted(list(set(metadata_file.parent for metadata_file in metadata_files)))
+    metadata_files = (
+        list(charms_dir.glob("*/metadata.yaml"))
+        + list(charms_dir.glob("*/metadata.yml"))
+        + list(charms_dir.glob("*/charmcraft.yaml"))
+    )
+
+    if not metadata_files:
+        # No nested charm directories found, check if the top level dir is
+        # a charm
+        metadata_files = (
+            list(root_dir.glob("metadata.yaml"))
+            + list(root_dir.glob("metadata.yml"))
+            + list(root_dir.glob("charmcraft.yaml"))
+        )
+
+    # Return the parent directory of the relevant metadata_files and
+    # remove duplicates
+    parent_dirs = [metadata_file.parent for metadata_file in metadata_files]
+    charm_dirs = sorted(list(set(parent_dirs)))
 
     return charm_dirs
 
+
 def stringify_paths(paths):
-	"""Converts list of Paths to list of strings, appending a trailing slash just in case"""
-	return [str(path) + "/" for path in paths]
+    """
+    Converts list of Paths to list of strings,
+    appending a trailing slash just in case.
+    """
+    return [str(path) + "/" for path in paths]
+
 
 def set_github_output(name, value):
     os.system(f'echo "{name}={value}" >> $GITHUB_OUTPUT')
 
+
 def emit_to_github_action(name, data):
-	"""Emits data to STDOUT as an output named name in a format that GitHub Actions consumes"""
-	data = str(data)
-	print(f"Found {name}: {data}")
-	set_github_output(name, data)
+    """
+    Emits data to STDOUT as an output named name in a format that GitHub
+    Actions consumes
+    """
+    data = str(data)
+    print(f"Found {name}: {data}")
+    set_github_output(name, data)
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
-		description="Find charm directories.  Searches for either nested charms or whether the "
-					"directory is itself a charm, printing a Github Action compatible output of a "
-					"JSON list of the found charm directories"
-	)
+    parser = argparse.ArgumentParser(
+        description="Find charm directories. Searches for either nested "
+        "charms  or whether the directory is itself a charm, printing a "
+        "Github Action compatible output of a JSON list of the found charm "
+        "directories"
+    )
 
-	parser.add_argument("base_dir", help="Directory to search for charms")
-	parser.add_argument("--charms-subdir", default="charms", help="Subdirectory to search for charms")
-	args = parser.parse_args()
-	charm_dirs = find_charms_in_dir(args.base_dir, args.charms_subdir)
-	charm_dirs = stringify_paths(charm_dirs)
-	emit_to_github_action(name=OUTPUT_VARIABLE_NAME, data=charm_dirs)
+    parser.add_argument("base_dir", help="Directory to search for charms")
+    parser.add_argument(
+        "--charms-subdir", default="charms", help="Subdirectory to search for charms"
+    )
+    args = parser.parse_args()
+    charm_dirs = find_charms_in_dir(args.base_dir, args.charms_subdir)
+    charm_dirs = stringify_paths(charm_dirs)
+    emit_to_github_action(name=OUTPUT_VARIABLE_NAME, data=charm_dirs)

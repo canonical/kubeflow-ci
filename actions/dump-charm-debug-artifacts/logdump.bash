870c6ed02d464a699420c00f0de54e6b8828cfaf
Dario Faccin <dario.faccin@canonical.com>
2026-05-21 12:14:04 +0200

    cleanup

--- actions/dump-charm-debug-artifacts/logdump.bash
+++ actions/dump-charm-debug-artifacts/logdump.bash
@@ -49,11 +49,11 @@ echo "Dumping logs to ${OUTPUT_DIR}"
 shopt -s nullglob
 # Common for most installs
 for f in $HOME/snap/charmcraft/common/cache/charmcraft/log/charmcraft-*.log; do
-    cat $f | tee "$OUTPUT_DIR/`basename $f`"
+    cat $f > "$OUTPUT_DIR/$(basename $f)"
 done
 # A spot sometimes seen on a gh runner
 for f in $HOME/.local/state/charmcraft/log/charmcraft-*.log; do
-    cat $f | tee "$OUTPUT_DIR/`basename $f`"
+    cat $f > "$OUTPUT_DIR/$(basename $f)"
 done
 shopt -u nullglob
 
@@ -66,22 +66,23 @@ juju-crashdump -o "$OUTPUT_DIR"
 
 # Collect `juju show-status-log` for every model, unit, and application
 # Expand $model and $unit with a substitution that replaces '/' with '-'
-for model in `juju list-models --format yaml | yq e '.models[].name'`; do
-    echo juju show-status-log --days 1 --type model $model | tee "$OUTPUT_DIR/juju-status-logs-model-${model//\//-}.txt"
-    juju show-status-log --days 1 --type model $model | tee "$OUTPUT_DIR/juju-status-logs-model-${model//\//-}.txt"
-
-	for application in `juju status -m $model --format yaml | yq e '.applications | keys | .[]'`; do
-		echo juju show-status-log --days 1 --type application $application | tee "$OUTPUT_DIR/juju-status-logs-application-$application.txt"
-		juju show-status-log --days 1 --type application $application | tee "$OUTPUT_DIR/juju-status-logs-application-$application.txt"
-		pod=$(kubectl get pod -n kubeflow -l app.kubernetes.io/name=$application -o jsonpath='{.items[0].metadata.name}') || continue
-		containers=($(kubectl get pod -n $model $pod -o jsonpath='{.spec.containers[*].name}'))
+for model in $(juju list-models --format yaml | yq e '.models[].short-name'); do
+    juju show-status-log --model $model --days 1 --type model $model > "$OUTPUT_DIR/juju-status-logs-model-$model.txt"
+	for application in $(juju status --model=$model --format yaml | yq e '.applications | keys | .[]'); do
+		juju show-status-log --model $model --days 1 --type application $application > "$OUTPUT_DIR/juju-status-logs-application-$application.txt"
+		k8s_model=$model
+		if [ $model == controller ]; then
+		    controller_name=$(juju status --model=$model --format yaml | yq e '.model.controller' )
+		    k8s_model=$model-$controller_name
+		fi
+		pod=$(kubectl get pod -n $k8s_model -l app.kubernetes.io/name=$application -o jsonpath='{.items[0].metadata.name}') || continue
+		containers=($(kubectl get pod -n $k8s_model $pod -o jsonpath='{.spec.containers[*].name}'))
 		for container in ${containers[@]}; do
-			kubectl logs -n $model $pod -c $container | tee "$OUTPUT_DIR/kubectl-logs-$application-$container.txt"
+			kubectl logs -n $k8s_model $pod -c $container > "$OUTPUT_DIR/kubectl-logs-$application-$container.txt"
 		done
 	done
-	for unit in `juju status -m $model --format yaml | yq e '.applications[].units | keys | .[]'`; do
-        echo juju show-status-log --days 1 --type unit $unit | tee "$OUTPUT_DIR/juju-status-logs-unit-${unit//\//-}.txt"
-        juju show-status-log --days 1 --type unit $unit | tee "$OUTPUT_DIR/juju-status-logs-unit-${unit//\//-}.txt"
+	for unit in $(juju status --model=$model --format yaml | yq e '.applications[].units | keys | .[]'); do
+        juju show-status-log --model=$model --days 1 --type unit $unit > "$OUTPUT_DIR/juju-status-logs-unit-${unit//\//-}.txt"
 	done
 done	
 
